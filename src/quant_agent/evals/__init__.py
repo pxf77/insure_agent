68a1f9ec8d95a58e36408f6aca4d014d8600105b
pxf77 <97281763+pxf77@users.noreply.github.com>
2026-07-11 17:44:23 +0800

    feat(evals): expose contract evaluation API

---
+++ src/quant_agent/evals/__init__.py
@@ -0,0 +1,17 @@
+from quant_agent.evals.contracts import (
+    ContractEvalCase,
+    ContractEvalOutcome,
+    ContractEvalReport,
+    load_contract_cases,
+    render_contract_eval_report,
+    run_contract_evals,
+)
+
+__all__ = [
+    "ContractEvalCase",
+    "ContractEvalOutcome",
+    "ContractEvalReport",
+    "load_contract_cases",
+    "render_contract_eval_report",
+    "run_contract_evals",
+]
